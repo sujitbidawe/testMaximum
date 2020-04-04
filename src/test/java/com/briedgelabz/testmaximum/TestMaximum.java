7da36b035638df8fa81c86c23c1536adda20ce89
sujitbidawe <bidawesujit@gmail.com>
2020-04-04 17:06:16 +0530

    refactored test cases to check with one generic method

--- src/test/java/com/briedgelabz/testmaximum/TestMaximum.java
+++ src/test/java/com/briedgelabz/testmaximum/TestMaximum.java
@@ -11,71 +11,71 @@ public class TestMaximum {
     @Test
     public void givenMaxIntegerAtFirstPosition_shouldReturnFirstPosition() {
         FindMaximum findMaximum = new FindMaximum();
-        int maxInt = findMaximum.findMaxInt(22, 2, 3);
-        Assert.assertEquals(22, maxInt);
-        System.out.println("Max number: " + maxInt);
+        Comparable maximumValue = findMaximum.findMaximum(22,2,3);
+        Assert.assertEquals(22, maximumValue);
+        System.out.println("Max number: " + maximumValue);
     }
 
     @Test
     public void givenMaxIntegerAtSecondPosition_shouldReturnSecondPosition() {
         FindMaximum findMaximum = new FindMaximum();
-        int maxInt = findMaximum.findMaxInt(1, 22, 3);
-        Assert.assertEquals(22, maxInt);
-        System.out.println("Max number: " + maxInt);
+        Comparable maximumValue = findMaximum.findMaximum(1,22,3);
+        Assert.assertEquals(22, maximumValue);
+        System.out.println("Max number: " + maximumValue);
     }
 
     @Test
     public void givenMaxIntegerAtThirdPosition_shouldReturnThirdPosition() {
         FindMaximum findMaximum = new FindMaximum();
-        int maxInt = findMaximum.findMaxInt(1, 2, 22);
-        Assert.assertEquals(22, maxInt);
-        System.out.println("Max number: " + maxInt);
+        Comparable maximumValue = findMaximum.findMaximum(1,2,22);
+        Assert.assertEquals(22, maximumValue);
+        System.out.println("Max number: " + maximumValue);
     }
 
     @Test
     public void givenMaxFloatAtFirstPosition_shouldReturnFirstPosition() {
         FindMaximum findMaximum = new FindMaximum();
-        double maxFloat = findMaximum.findMaxFloat(22.1, 2.1, 3.1);
-        Assert.assertEquals(22.1, maxFloat, 0.0);
-        System.out.println("Max number: " + maxFloat);
+        Comparable maximumValue = findMaximum.findMaximum(22.1,1.1,3.1);
+        Assert.assertEquals(22.1, maximumValue);
+        System.out.println("Max number: " + maximumValue);
     }
 
     @Test
-    public void givenMaxFloatAtSecondPosition_shouldReturnFirstPosition() {
+    public void givenMaxFloatAtSecondPosition_shouldReturnSecondPosition() {
         FindMaximum findMaximum = new FindMaximum();
-        double maxFloat = findMaximum.findMaxFloat(1.1, 22.1, 3.1);
-        Assert.assertEquals(22.1, maxFloat, 0.0);
-        System.out.println("Max number: " + maxFloat);
+        Comparable maximumValue = findMaximum.findMaximum(1.1,22.1,3.1);
+        Assert.assertEquals(22.1, maximumValue);
+        System.out.println("Max number: " + maximumValue);
     }
 
     @Test
-    public void givenMaxFloatAtThirdPosition_shouldReturnFirstPosition() {
+    public void givenMaxFloatAtThirdPosition_shouldReturnThirdPosition() {
         FindMaximum findMaximum = new FindMaximum();
-        double maxFloat = findMaximum.findMaxFloat(1.1, 2.1, 22.1);
-        Assert.assertEquals(22.1, maxFloat, 0.0);
-        System.out.println("Max number: " + maxFloat);
+        Comparable maximumValue = findMaximum.findMaximum(1.1,2.1,22.1);
+        Assert.assertEquals(22.1, maximumValue);
+        System.out.println("Max number: " + maximumValue);
     }
 
     @Test
     public void givenMaxStringAtFirstPosition_shouldReturnFirstPosition() {
         FindMaximum findMaximum = new FindMaximum();
-        String maxString = findMaximum.findMaxString("peach", "apple", "banana");
+        String maxString = (String) findMaximum.findMaximum("peach", "apple", "banana");
         Assert.assertEquals("peach", maxString);
         System.out.println("Max string: "+ maxString);
     }
 
     @Test
-    public void givenMaxStringAtSecondPosition_shouldReturnFirstPosition() {
+    public void givenMaxStringAtSecondPosition_shouldReturnSecondPosition() {
         FindMaximum findMaximum = new FindMaximum();
-        String maxString = findMaximum.findMaxString("apple", "peach", "banana");
+        String maxString = (String) findMaximum.findMaximum("apple", "peach", "banana");
         Assert.assertEquals("peach", maxString);
         System.out.println("Max string: "+ maxString);
     }
 
     @Test
-    public void givenMaxStringAtThirdPosition_shouldReturnFirstPosition() {
+    public void givenMaxStringAtThirdPosition_shouldReturnThirdPosition() {
         FindMaximum findMaximum = new FindMaximum();
-        String maxString = findMaximum.findMaxString("apple", "banana", "peach");
+        String maxString = (String) findMaximum.findMaximum("apple", "banana", "peach");
         Assert.assertEquals("peach", maxString);
         System.out.println("Max string: "+ maxString);
     }
